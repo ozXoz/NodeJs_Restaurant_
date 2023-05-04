c7462921b24fc5e4d603393c0702f39d30b2e6e7
ozXoz <honourkorkmaz@gmail.com>
2023-05-04 16:15:30 -0400

    bug2

--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com',
-  user: 'uaortdlriui4dpmj',
-  password: 'MKJIiQsq8vARALQVgYDL',
-  database: 'buup8iufmzqdwxmgag1r'
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, 
+  user: process.env.DB_USERNAME, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DBNAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-module.exports = connection;
+pool.getConnection((err, conn) => {
+  if(err) console.log(err)
+  console.log("Connected successfully")
+})
+
+module.exports = pool.promise()
