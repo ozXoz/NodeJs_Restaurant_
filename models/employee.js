f94c7470a1c88112fe9f9b81f543cf5470b37b60
ozXoz <honourkorkmaz@gmail.com>
2023-05-04 15:12:13 -0400

    api

--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com',
-  user: 'uaortdlriui4dpmj',
-  password: 'MKJIiQsq8vARALQVgYDL',
-  database: 'buup8iufmzqdwxmgag1r'
+  host: 'host',
+  user: 'user',
+  password: 'password',
+  database: 'database'
 });
+
 
 module.exports = connection;
