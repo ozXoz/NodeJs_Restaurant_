5b71566342b02eee972f3dc2253e5be18b37c624
ozXoz <honourkorkmaz@gmail.com>
2023-05-04 15:24:01 -0400

    update database

--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com',
-  user: 'uaortdlriui4dpmj',
-  password: 'MKJIiQsq8vARALQVgYDL',
-  database: 'buup8iufmzqdwxmgag1r'
+  MYSQL_ADDON_HOST:"buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com",
+  MYSQL_ADDON_DB:"buup8iufmzqdwxmgag1r",
+  MYSQL_ADDON_USER:"uaortdlriui4dpmj",
+  MYSQL_ADDON_PORT:"3306",
+  MYSQL_ADDON_PASSWORD:"MKJIiQsq8vARALQVgYDL"
+  // MYSQL_ADDON_URI=mysql://uaortdlriui4dpmj:MKJIiQsq8vARALQVgYDL@buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com:3306/buup8iufmzqdwxmgag1r
+  
 });
 
 module.exports = connection;
