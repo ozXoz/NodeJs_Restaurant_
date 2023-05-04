f4993bb569e56f9715e3800d7401cfa864e564ab
ozXoz <honourkorkmaz@gmail.com>
2023-05-04 15:32:03 -0400

    database fix

--- models/employee.js
+++ models/employee.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  MYSQL_ADDON_HOST:"buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com",
-  MYSQL_ADDON_DB:"buup8iufmzqdwxmgag1r",
-  MYSQL_ADDON_USER:"uaortdlriui4dpmj",
-  MYSQL_ADDON_PORT:"3306",
-  MYSQL_ADDON_PASSWORD:"MKJIiQsq8vARALQVgYDL"
-  // MYSQL_ADDON_URI=mysql://uaortdlriui4dpmj:MKJIiQsq8vARALQVgYDL@buup8iufmzqdwxmgag1r-mysql.services.clever-cloud.com:3306/buup8iufmzqdwxmgag1r
-  
+  host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+connection.getConnection((err, conn) => {
+  if(err) console.log(err)
+  console.log("Connected successfully")
+})
 
 module.exports = connection;
